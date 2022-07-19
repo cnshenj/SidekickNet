927100a60549b1a39a360bdb21109d2ac0fbc5d3
cnshenj <cnshenj@gmail.com>
2022-07-18 18:40:45 -0700

    Remove possible multiple enumerations

--- Utilities.Test/CollectionTest.cs
+++ Utilities.Test/CollectionTest.cs
@@ -9,9 +9,9 @@ namespace SidekickNet.Utilities.Test
         [Fact]
         public void Create_Partitions_From_Enumerable()
         {
-            var source = Enumerable.Range(1, 10);
+            var source = Enumerable.Range(1, 10).ToArray();
             const int size = 3;
-            var partitions = source.ToPartitions(size);
+            var partitions = source.ToPartitions(size).ToArray();
 
             // All partitions, except the last one should have exactly 'size' number of elements
             Assert.True(partitions.SkipLast(1).All(p => p.Count() == size));
