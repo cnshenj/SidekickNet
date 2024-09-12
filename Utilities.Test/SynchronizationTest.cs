556c6b6425957a163813cbfafba9492c520e9322
cnshenj <cnshenj@gmail.com>
2024-09-12 14:49:22 -0700

    Add more synchronization tests

--- Utilities.Test/SynchronizationTest.cs
+++ Utilities.Test/SynchronizationTest.cs
@@ -1,10 +1,10 @@
 using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using SidekickNet.Utilities.Synchronization;
 using Xunit;
-
-using Convert = SidekickNet.Utilities.BasicConvert;
 
 namespace SidekickNet.Utilities.Test
 {
@@ -48,17 +48,19 @@ namespace SidekickNet.Utilities.Test
         public void Get_Lock_From_Factory()
         {
             var factory = new AccessLockFactory<string>(_ => new LocalSemaphore(1, 1));
-            using var @lock = factory.GetLock("foobar");
-            Assert.True(@lock.Acquired);
+            using var lock1 = factory.GetLock("foo");
+            using var lock2 = factory.GetLock("bar");
+            Assert.True(lock1.Acquired);
+            Assert.True(lock2.Acquired);
         }
 
         [Fact]
-        public async Task Get_Lock_Again_Throw_Exception()
+        public void Get_Lock_Again_Throw_Exception()
         {
             const string key = "foobar";
             var factory = new AccessLockFactory<string>(_ => new LocalSemaphore(1, 1));
             using var @lock = factory.GetLock(key);
-            await Task.Run(() => Assert.Throws<TimeoutException>(() => factory.GetLock(key, TimeSpan.Zero)));
+            Assert.Throws<TimeoutException>(() => factory.GetLock(key, TimeSpan.Zero));
         }
 
         [Fact]
@@ -105,8 +107,10 @@ namespace SidekickNet.Utilities.Test
         public async Task Get_Lock_Async_From_Factory()
         {
             var factory = new AccessLockFactory<string>(_ => new LocalSemaphore(1, 1));
-            await using var @lock = await factory.GetLockAsync("foobar");
-            Assert.True(@lock.Acquired);
+            await using var lock1 = await factory.GetLockAsync("foo");
+            await using var lock2 = await factory.GetLockAsync("bar");
+            Assert.True(lock1.Acquired);
+            Assert.True(lock2.Acquired);
         }
 
         [Fact]
@@ -115,8 +119,7 @@ namespace SidekickNet.Utilities.Test
             const string key = "foobar";
             var factory = new AccessLockFactory<string>(_ => new LocalSemaphore(1, 1));
             await using var @lock = await factory.GetLockAsync(key);
-            await Task.Run(async () =>
-                await Assert.ThrowsAsync<TimeoutException>(async () => await factory.GetLockAsync(key, TimeSpan.Zero)));
+            await Assert.ThrowsAsync<TimeoutException>(async () => await factory.GetLockAsync(key, TimeSpan.Zero));
         }
 
         [Fact]
@@ -124,12 +127,28 @@ namespace SidekickNet.Utilities.Test
         {
             const string key = "foobar";
             var factory = new AccessLockFactory<string>(_ => new LocalSemaphore(1, 1));
-            using var @lock = factory.GetLock(key);
+            await using var @lock = await factory.GetLockAsync(key);
             await Task.Run(async () =>
             {
                 await using var lockAgain = await factory.TryGetLockAsync(key, TimeSpan.Zero);
                 Assert.False(lockAgain.Acquired);
             });
+        }
+
+        [Fact]
+        public async Task Try_Get_Lock_Concurrently_Only_One_Acquired()
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            const string key = "foobar";
+            var factory = new AccessLockFactory<string>(_ => new LocalSemaphore(1, 1));
+            var task1 = factory.TryGetLockAsync(key, TimeSpan.FromMilliseconds(100));
+            var task2 = factory.TryGetLockAsync(key, TimeSpan.FromMilliseconds(100));
+            var locks = await Task.WhenAll(task1, task2);
+            var locked = locks.Where(l => l.Acquired);
+
+            // Only 1 lock should be acquired concurrently
+            Assert.Single(locked);
         }
 
         [Fact]
